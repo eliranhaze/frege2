9eb790cb137600279fe7ea6c0abd3912378f7f8b
Eliran Haziza <eliranhaze@gmail.com>
2016-06-03 15:40:03 +0300

    added models

--- frege/polls/models.py
+++ frege/polls/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
