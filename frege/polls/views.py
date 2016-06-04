bf45f2241e9c4f4f16a14bf6bcaf9b8ec29444e0
Eliran Haziza <eliranhaze@gmail.com>
2016-06-04 11:29:16 +0300

    view template

--- frege/polls/views.py
+++ frege/polls/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, universe!!! You're at the polls index.")
+    qlist = Question.objects.order_by('date')[:10]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'qlist' : qlist,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, qid):
     return HttpResponse("You're looking at the results of question %s." % qid) 
