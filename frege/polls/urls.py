8ada693024ec63b5bf4c444b5057a4645de2e3a1
Eliran Haziza <eliranhaze@gmail.com>
2016-06-04 11:00:49 +0300

    added views to polls

--- frege/polls/urls.py
+++ frege/polls/urls.py
@@ -2,6 +2,15 @@ from django.conf.urls import url
 
 from . import views
 
+QID_EXP = '(?P<qid>[0-9]+)'
+
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/ 
+    url(r'^%s/$' % QID_EXP, views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^%s/results/$' % QID_EXP, views.detail, name='detail'),
+    # ex: /polls/5/vote/ 
+    url(r'^%s/vote/$' % QID_EXP, views.detail, name='detail'),
 ]
--- frege/polls/views.py
+++ frege/polls/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, universe!!! You're at the polls index.")
+    return HttpResponse("Hello, universe!!! You're at the polls index.")
 
+def detail(request, qid):
+    return HttpResponse("You're looking at the results of question %s." % qid) 
+
+def vote(request, qid):
+    return HttpResponse("You're voting on question %s." % qid) 
