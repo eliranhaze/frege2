68a4b33d7a863c5ce36f74d94f35e5c0046931dd
Eliran Haziza <eliranhaze@gmail.com>
2016-06-10 00:04:35 +0300

    initial view tests

--- frege/polls/tests.py
+++ frege/polls/tests.py
@@ -1,9 +1,23 @@
 from datetime import datetime, timedelta
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
+
+def create_question(text, days):
+    time = timezone(now) + timedelta(days=days)
+    return Question.objects.create(text=text, date=time)
+
+class QuestionViewTests(TestCase):
+
+    def test_index_view_no_questions(self):
+        """ should display message when no questions exists"""
+        res = self.client.get(reverse('polls:index'))
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, 'No polls are available')
+        self.assertQuerysetEqual(res.context['qlist'], [])
 
 class QuestionTests(TestCase):
 
