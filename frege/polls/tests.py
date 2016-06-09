07978db425d48d0bf9e0985b67d7c05ddfeb2729
Eliran Haziza <eliranhaze@gmail.com>
2016-06-10 00:48:23 +0300

    more index view tests

--- frege/polls/tests.py
+++ frege/polls/tests.py
@@ -7,17 +7,52 @@ from django.utils import timezone
 from .models import Question
 
 def create_question(text, days):
-    time = timezone(now) + timedelta(days=days)
-    return Question.objects.create(text=text, date=time)
-
+     time = timezone.now() + timedelta(days=days)
+     return Question.objects.create(text=text, date=time)
+ 
 class QuestionViewTests(TestCase):
+ 
+    def _get(self):
+        return self.client.get(reverse('polls:index'))
 
+    def _assertEmpty(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available')
+        self.assertQuerysetEqual(response.context['qlist'], [])
+
+    def _assertContains(self, response, questions):
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['qlist'],
+            [repr(q) for q in questions],
+            ordered = False
+        )
+        
     def test_index_view_no_questions(self):
-        """ should display message when no questions exists"""
-        res = self.client.get(reverse('polls:index'))
-        self.assertEqual(res.status_code, 200)
-        self.assertContains(res, 'No polls are available')
-        self.assertQuerysetEqual(res.context['qlist'], [])
+        """should display message when no questions exists"""
+        self._assertEmpty(self._get())
+
+    def test_index_view_future_question(self):
+        """should display no future question"""
+        create_question('future', days=30)
+        self._assertEmpty(self._get())
+
+    def test_index_view_past_question(self):
+        """should display past question"""
+        q = create_question('past', days=-30)
+        self._assertContains(self._get(), [q])
+
+    def test_index_view_past_and_future(self):
+        """should display past question"""
+        q = create_question('past', days=-30)
+        create_question('future', days=30)
+        self._assertContains(self._get(), [q])
+
+    def test_index_view_two_past(self):
+        """should display past question"""
+        q = create_question('past', days=-30)
+        q2 = create_question('past2', days=-10)
+        self._assertContains(self._get(), [q, q2])
 
 class QuestionTests(TestCase):
 
