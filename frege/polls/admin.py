399ae5a8ddc9818f27853d1134e59aaad6d411fa
Eliran Haziza <eliranhaze@gmail.com>
2016-06-10 12:09:28 +0300

    more admin power!

--- frege/polls/admin.py
+++ frege/polls/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
 #    fields = ['date', 'text']
@@ -8,6 +12,10 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields':['text']}),
         ('Date information', {'fields':['date']}),
     ] 
+    inlines = [ChoiceInline]
+    list_display = ('text', 'date', 'was_published_recently')
+    list_filter = ['date']
+    search_fields = ['text']
 
 
 admin.site.register(Question, QuestionAdmin)
--- frege/polls/models.py
+++ frege/polls/models.py
@@ -15,6 +15,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now > self.date > now - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'פורסם לאחרונה?'
 
     def __unicode__(self):
         return self.text
