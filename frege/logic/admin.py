b80de08fc0b62c4cb27b69f62303b731a74d6364
Eliran Haziza <eliranhaze@gmail.com>
2016-06-11 20:51:31 +0300

    inline models in admin

--- frege/logic/admin.py
+++ frege/logic/admin.py
@@ -7,11 +7,60 @@ from .models import (
     ChoiceQuestion,
     TruthTableQuestion,
     DeductionQuestion,
+    FormulationAnswer,
+    Choice,
 )
 
-admin.site.register(Chapter)
+class FormulationAnswerInline(admin.StackedInline):
+    model = FormulationAnswer
+    extra = 0
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class OpenQuestionInline(admin.StackedInline):
+    model = OpenQuestion
+    extra = 0
+
+class FormulationQuestionInline(admin.StackedInline):
+    model = FormulationQuestion
+    extra = 0
+
+class ChoiceQuestionInline(admin.StackedInline):
+    model = ChoiceQuestion
+    extra = 0
+
+class TruthTableQuestionInline(admin.StackedInline):
+    model = TruthTableQuestion
+    extra = 0
+
+class DeductionQuestionInline(admin.StackedInline):
+    model = DeductionQuestion
+    extra = 0
+
+class FormulationQuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        FormulationAnswerInline,
+    ]
+
+class ChoiceQuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        ChoiceInline,
+    ]
+
+class ChapterAdmin(admin.ModelAdmin):
+    inlines = [
+        OpenQuestionInline,
+        FormulationQuestionInline,
+        ChoiceQuestionInline,
+        TruthTableQuestionInline,
+        DeductionQuestionInline,
+    ]
+
+admin.site.register(Chapter, ChapterAdmin)
 admin.site.register(OpenQuestion)
-admin.site.register(FormulationQuestion)
-admin.site.register(ChoiceQuestion)
+admin.site.register(FormulationQuestion, FormulationQuestionAdmin)
+admin.site.register(ChoiceQuestion, ChoiceQuestionAdmin)
 admin.site.register(TruthTableQuestion)
 admin.site.register(DeductionQuestion)
