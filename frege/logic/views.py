6fcf3193a40abcf2472945eb730737925f09d286
Eliran Haziza <eliranhaze@gmail.com>
2016-06-12 16:25:23 +0300

    added chapter detail view

--- frege/logic/views.py
+++ frege/logic/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views import generic
 
-from .models import Chapter
+from .models import (
+    Chapter,
+    ChoiceQuestion,
+)
 
 class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = 'logic/index.html'
@@ -13,9 +16,14 @@ class IndexView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         return Chapter.objects.all()
 
-class ChapterView(LoginRequiredMixin, generic.ListView):
-    template_name = 'logic/index.html'
+class ChapterView(LoginRequiredMixin, generic.DetailView):
+    template_name = 'logic/chapter.html'
 
-    def get_queryset(self):
-        return []
+    def get_object(self):
+        return get_object_or_404(Chapter, number=self.kwargs['number'])
+
+    def get_context_data(self, **kwargs):
+        context = super(ChapterView, self).get_context_data(**kwargs)
+        context['question_list'] = ChoiceQuestion.objects.filter(chapter__number=self.kwargs['number'])
+        return context
 
