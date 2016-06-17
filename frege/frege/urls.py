18d271034bd92ae6ff79d89a8605871a21b0e44f
Eliran Haziza <eliranhaze@gmail.com>
2016-06-17 11:58:36 +0300

    navbar and some url fixes

--- frege/frege/urls.py
+++ frege/frege/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^polls/', include('polls.urls')),
 
     # account stuff
-    url(r'^accounts/login/$', login),
-    url(r'^accounts/logout/$', logout),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
     url(r'^accounts/register/$', register),
 ]
