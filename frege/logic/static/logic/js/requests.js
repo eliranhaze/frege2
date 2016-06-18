5160893c1d3d70c74c41c1b786003bcad4bc7770
Eliran Haziza <eliranhaze@gmail.com>
2016-06-18 14:41:55 +0300

    moved js code to separate file

---
+++ frege/logic/static/logic/js/requests.js
@@ -0,0 +1,71 @@
+function post_answer(url, data, csrf) {
+    if (!$('input[name=choice]:checked', '#cq').val()) {
+        $.notify({
+            icon: "glyphicon glyphicon-pencil",
+            message: "<strong>לא נבחרה תשובה</strong>"
+        },{
+            type: "warning",
+            delay: 1000,
+        });
+        return false
+    }
+    $("#answer").html("בודק...");
+    data['csrfmiddlewaretoken'] = csrf;
+    $.post(url, data, function(data, status) {
+        if (data['correct']) {
+            $.notify({
+                icon: "glyphicon glyphicon-ok",
+                message: "<strong>תשובה נכונה</strong>"
+            },{
+                type: "success",
+            });
+            $("#answer").html("המשך");
+            $("#answer").off("click");
+            $("#answer").attr("onclick", data['next_url']);
+            $("#answer").attr("class", "btn btn-primary btn-lg");
+        } else {
+            $.notify({
+                icon: "glyphicon glyphicon-remove",
+                message: "<strong>תשובה שגויה</strong>"
+            },{
+                type: "danger",
+            });
+            $("#answer").html("אישור");
+        }
+    })
+    .fail(function(response){
+        if (response.responseText) {
+            $.notifyClose();
+            $.notify({
+                message: "<strong>נראה שיש בעיה בשרת...</strong> \
+                         <br>הבעיה נרשמה והיא תטופל בהקדם. \
+                         <br>תודה על הסבלנות. \
+                         <br>בינתיים, הנה לב ממני: <span class='glyphicon glyphicon-heart'></span>"
+            },{
+                type: "info",
+                allow_dismiss: true,
+                newest_on_top: true,
+                delay: 15000,
+            });
+            $("#answer").html("אישור");
+        } else {
+            $.notify({
+                icon: "glyphicon glyphicon-flash",
+                message: "<strong>בעיה בהתחברות לשרת</strong> אנא בדקו את החיבור לאינטרנט"
+            },{
+                type: "warning",
+            });
+            $("#answer").html("אישור");
+        }
+    });
+}
+
+$(document).ready(function(){
+    $.notifyDefaults({
+        allow_dismiss: false,
+        offset: 100,
+        placement: {from: "bottom", align: "center"},
+        animate: {enter: "animated fadeIn", exit: "animated fadeOut"},
+        delay: 2000,
+    });
+});
