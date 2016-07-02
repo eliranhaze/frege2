9a1e200421387efdb40453afeab36efc80d2163c
Eliran Haziza <eliranhaze@gmail.com>
2016-07-03 00:23:57 +0300

    added logical symbol buttons for user input

--- frege/logic/static/logic/js/deduction.js
+++ frege/logic/static/logic/js/deduction.js
@@ -328,7 +328,7 @@ function errmsg(msg) {
     $("#errmsg").text(msg);
 }
 function showText(btn) {
-    $("#extra").show();
+    $("#extra").css("visibility", "visible");
     $("#extxt").focus();
     $("#extxt").on("input", function() {
         if (!btn.data("symbol")) {
@@ -346,7 +346,7 @@ function showText(btn) {
     });
 }
 function hideText(btn) {
-    $("#extra").hide();
+    $("#extra").css("visibility", "hidden");
     $("#extxt").val("");
     $("#extxt").off("input");
     $(document).off("keypress");
@@ -360,3 +360,54 @@ function hideText(btn) {
 function getText() {
     return $("#extxt").val();
 }
+
+// function for inserting text at cursor position
+jQuery.fn.extend({
+    insertAtCaret: function(myValue){
+        return this.each(function(i) {
+            if (document.selection) {
+                //For browsers like Internet Explorer
+                this.focus();
+                var sel = document.selection.createRange();
+                sel.text = myValue;
+                this.focus();
+            } else if (this.selectionStart || this.selectionStart == '0') {
+                //For browsers like Firefox and Webkit based
+                var startPos = this.selectionStart;
+                var endPos = this.selectionEnd;
+                var scrollTop = this.scrollTop;
+                this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+                this.focus();
+                this.selectionStart = startPos + myValue.length;
+                this.selectionEnd = startPos + myValue.length;
+                this.scrollTop = scrollTop;
+            } else {
+                this.value += myValue;
+                this.focus();
+            }
+        });
+    }
+});
+function insert(text) {
+    $("#extxt").insertAtCaret(text);
+    $("#extxt").focus();
+}
+
+// bind symbol buttons to insertions
+$(document).ready(function() {
+    $("#neg").click(function() {
+        insert('~');
+    });
+    $("#con").click(function() {
+        insert('·');
+    });
+    $("#dis").click(function() {
+        insert('∨');
+    });
+    $("#imp").click(function() {
+        insert('⊃');
+    });
+    $("#eqv").click(function() {
+        insert('≡');
+    })
+});
