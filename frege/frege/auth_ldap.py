25450c0340ad73cbb58c19d22beaed8b371e868c
Eliran Haziza <eliranhaze@gmail.com>
2016-09-30 14:12:59 +0300

    get_ou in ldap

--- frege/frege/auth_ldap.py
+++ frege/frege/auth_ldap.py
@@ -17,26 +17,35 @@ def auth(uname, pw):
     if not ENABLED:
         return True
     success = False
-    for ou in USER_OU:
-        try:
-            connect().simple_bind_s('cn=%s,ou=%s,o=TAU' % (uname, ou), pw)
-            success = True
-            break
-        except ldap.INVALID_CREDENTIALS:
-            pass
+    try:
+        connect().simple_bind_s('cn=%s,ou=%s,o=TAU' % (uname, get_user_ou(uname)), pw)
+        success = True
+    except ldap.INVALID_CREDENTIALS:
+        pass
     return success
 
 def user_exists(uname):
     if not ENABLED:
         return True
     for ou in USER_OU:
-        result = connect().search_s('ou=%s,o=TAU' % ou, ldap.SCOPE_SUBTREE, 'cn=%s' % uname)
-        if result:
+        if user_exists_in_ou(uname, ou):
             return True
     return False
+
+def user_exists_in_ou(uname, ou):
+    if not ENABLED:
+        return True
+    result = connect().search_s('ou=%s,o=TAU' % ou, ldap.SCOPE_SUBTREE, 'cn=%s' % uname)
+    return len(result) > 0
 
 def list_students(course_id=COURSE_ID, group_id=COURSE_MAIN):
     if not ENABLED:
         return []
     result = connect().search_s('ou=Courses,o=TAU', ldap.SCOPE_SUBTREE, 'cn=%s%s' % (course_id, group_id))
     return result
+
+def get_user_ou(uname):
+    if ENABLED:
+        for ou in USER_OU:
+            if user_exists_in_ou(uname, ou):
+                return ou
